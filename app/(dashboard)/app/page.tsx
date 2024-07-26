7c9dd396f2d8f80c318e52ff914b1e9eb3cfb81f
enriquepina <enrique.pina@sowifyglobalsolutions.com>
2024-07-26 09:11:32 +0200

    feat: make text page resizable

--- app/(dashboard)/app/page.tsx
+++ app/(dashboard)/app/page.tsx
@@ -1,15 +1,26 @@
 import FixedTextViewer from "@/components/fixed-text-viewer";
 import TextInput from "@/components/text-input";
-import { Separator } from "@/components/ui/separator";
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
 
 export default function Home() {
   return (
     <main className="relative flex flex-col w-full">
-      <div className="flex flex-col items-center md:items-start md:flex-row md:justify-evenly h-full w-full gap-4 p-4 ">
-        <TextInput classname="basis-1/2 h-full" />
-        <Separator orientation="vertical" />
-        <FixedTextViewer classname="basis-1/2 h-full" />
-      </div>
+      <ResizablePanelGroup
+        direction="horizontal"
+        className="flex flex-col items-center md:items-start md:flex-row md:justify-evenly h-full w-full gap-4 p-4 "
+      >
+        <ResizablePanel defaultSize={50} minSize={25} className="h-full p-1">
+          <TextInput classname="h-full" />
+        </ResizablePanel>
+        <ResizableHandle withHandle className="h-full" />
+        <ResizablePanel defaultSize={50} minSize={25} className="h-full">
+          <FixedTextViewer classname="h-full" />
+        </ResizablePanel>
+      </ResizablePanelGroup>
     </main>
   );
 }
